eba1a9d4221efc57e3705a352ef33bb5df109768
Mo2Hefny <moamensaied1@gmail.com>
2023-08-15 16:51:32 +0300

    Handle some operations and calculations

--- js/script.js
+++ js/script.js
@@ -5,10 +5,11 @@ const screen = document.querySelector("#screen");
 const buttons = document.querySelector(".buttons");
 const screenNumber = document.getElementById("screen-number");
 const screenEquation = document.getElementById("screen-equation");
-
+let equation = [0];
 let selectedOperator = "";
 let ans = "";
 let decimal = false;
+let newEntry = false;
 let decimalDigits = 0;
 
 buttons.addEventListener("click", handleUserInput);
@@ -21,16 +22,25 @@ function handleUserInput(event) {
   if (button.classList.contains("num-btn")) {
     handleNumberChange(button);
   }
+  else {
+    handleFunctions(button);
+  }
   console.log(button);
 }
 
 function handleNumberChange(button) {
   const ID = button.getAttribute("id");
+  if (newEntry) {
+    screenNumber.textContent = 0;
+    newEntry = false;
+  }
   let number = parseFloat(screenNumber.textContent.replace(/,/g, ""));
+  
   if (ID == "switch") number = -number;
-  else if (ID == "decimal" && !decimal) {
+  else if (ID == "decimal") {
     return toggleDecimalNumber();
-  } else {
+  }
+  else {
     return appendNumber(number, button.textContent);
   }
 
@@ -52,10 +62,105 @@ function appendNumber(number, digit) {
   if (decimal) {
     number = Number(screenNumber.textContent + digit);
     decimalDigits++;
-  } else {
+  }
+  else {
     number = Number(number + digit);
   }
   if (Math.abs(number) > MAX_NUMBER) return;
+  equation[equation.length - 1] = number;
   let commas = number.toLocaleString("en-US");
   screenNumber.textContent = commas;
+}
+
+function handleFunctions(button) {
+  const ID = button.getAttribute("id");
+  switch (ID) {
+    case "divide": handleOperations("/"); break;
+    case "multiply": handleOperations("*"); break;
+    case "add": handleOperations("+"); break;
+    case "subtract": handleOperations("-"); break;
+    case "result" : solveEquation(); break;
+    case "clearEntry" : clearEntry(); break;
+    case "clearAll" : clearAll(); break;
+  }
+}
+
+function handleOperations(operation) {
+  if (selectedOperator === operation) return;
+  selectedOperator = operation;
+  equation[1] = operation;
+  if (equation[2] == undefined)
+  {
+    newEntry = true;
+    decimal = false;
+    equation[2] = equation[0];
+  }
+  let commas = equation[0].toLocaleString("en-US") + " " + operation;
+  screenEquation.textContent = commas;
+}
+
+function solveEquation() {
+  decimal = false;
+  newEntry = true;
+  if (equation[1] == undefined) {
+    screenEquation.textContent = equation[0] + " =";
+    screenNumber.textContent = equation[0];
+    return addToHistory();
+  }
+  screenEquation.textContent += " " + equation[2] + " =";
+  switch (equation[1]) {
+    case "/": equation[0] = div(equation[0], equation[2]); break;
+    case "*": equation[0] = mul(equation[0], equation[2]); break;
+    case "+": equation[0] = add(equation[0], equation[2]); break;
+    case "-": equation[0] = sub(equation[0], equation[2]); break;
+  }
+  selectedOperator = "";
+  screenNumber.textContent = equation[0];
+  addToHistory();
+}
+
+function addToHistory() {
+  const section = document.createElement('div');
+  const equation = document.createElement('p');
+  const number = document.createElement('h1');
+
+  section.classList.add('section');
+  equation.classList.add('equation');
+  equation.textContent = screenEquation.textContent;
+  number.classList.add('number');
+  number.textContent = screenNumber.textContent;
+  section.append(equation);
+  section.append(number);
+  history.prepend(section);
+}
+
+function mul(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  return a / b;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function clearAll() {
+  screenEquation.textContent = "";
+  screenNumber.textContent = 0;
+  decimal = false;
+  newEntry = false;
+}
+
+function clearEntry() {
+  if (screenEquation.textContent === "") return;
+  if (screenEquation.textContent[-1] === "=") return clearAll();
+  screenNumber.textContent = 0;
+  decimal = false;
+  newEntry = false;
 }
